5f3fd2705849b772d264d8d598ad836dd9ff1793
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-06-28 21:04:42 +0100

    Main Frame class added

--- src/main/java/software/ulpgc/es/Main.java
+++ src/main/java/software/ulpgc/es/Main.java
@@ -3,6 +3,7 @@ package software.ulpgc.es;
 import software.ulpgc.es.model.CsvPlayerDeserializer;
 import software.ulpgc.es.model.FilePlayerLoader;
 import software.ulpgc.es.model.Player;
+import software.ulpgc.es.view.MainFrame;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,12 +16,7 @@ public class Main {
         File file = new File("D:/clase/Asignaturas_tercero/is2/kata3/dataset/players_data-2024_2025.csv");
         List<Player> players = new FilePlayerLoader(file, new CsvPlayerDeserializer()).load();
 
-        Map<String, String> data = new HashMap<>();
-        for (Player player: players) {
-            data.put(player.getName(), player.getNacionality());
-        }
-        for (String playerName: data.keySet()){
-            System.out.println(playerName + "\t" + data.get(playerName));
-        }
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
     }
 }
---
+++ src/main/java/software/ulpgc/es/view/MainFrame.java
@@ -0,0 +1,14 @@
+package software.ulpgc.es.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        this.setTitle("BarChart Football");
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
